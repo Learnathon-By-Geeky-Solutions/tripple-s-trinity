0e97d4c93f3e882282f4d345bff3231d265916ee
Mohammad I-K Sakur <mdiftakharkabirsakur@gmail.com>
2025-02-26 00:12:31 +0600

    readonly meele enemy

--- MechaMorph/Assets/Scripts/Enemy/MeleeEnemyAi.cs
+++ MechaMorph/Assets/Scripts/Enemy/MeleeEnemyAi.cs
@@ -4,7 +4,7 @@ namespace TrippleTrinity.MechaMorph.Enemy
 {
     public class MeleeEnemyAi : MonoBehaviour
     {
-        private int _damage= 1;
+        private readonly int _damage= 1;
          void OnParticleCollision(GameObject other)
         {
             Debug.Log($"Particle hit: {other.name}"); 
