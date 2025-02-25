d667d31d7cc7f5f90130cbeffc8a1e8e305ec84f
Mohammad I-K Sakur <mdiftakharkabirsakur@gmail.com>
2025-02-25 23:55:05 +0600

    Meele Enemy

--- MechaMorph/Assets/Scripts/Enemy/GrenadeSpawner.cs
+++ MechaMorph/Assets/Scripts/Enemy/GrenadeSpawner.cs
@@ -1,39 +1,23 @@
 
 using UnityEngine;
 using TrippleTrinity.MechaMorph.Health;  
-using TrippleTrinity.MechaMorph.Damage;
 namespace TrippleTrinity.MechaMorph.Enemy
 {
     public class GrenadeSpawner : MonoBehaviour
     {
-        private int _damage = 5;
+        private int _damage = 2;
         //grenade force
-        [SerializeField] private float _force = 100f;
-        [SerializeField]private float _radius = 2f;
         private void Start()
         {
-            //collision with particle effect
-            Explode();
             Destroy(this.gameObject,3f);
         }
 
-        void Explode()
-        {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, _radius);
-            foreach (Collider nearbyObject in colliders)
-            {
-                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    rb.AddExplosionForce(_force, transform.position, _radius);
-                }
-            }
-        }
+
 
         void OnParticleCollision(GameObject other)
         {
-            Debug.Log($"Particle hit: {other.name}"); // Debugging
-
+           
+            Debug.Log($"OnParticleCollision detected with: {other.name} (Tag: {other.tag})");
             if (other.CompareTag("Player")) 
             {
                 PlayerHealth playerHealth = other.GetComponent<PlayerHealth>() ?? other.GetComponentInParent<PlayerHealth>();
